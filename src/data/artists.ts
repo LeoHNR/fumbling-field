b10b6fdec3020722741a6d77ee1a34b825550980
Hector Reyes <hectorreyeskun@gmail.com>
2024-06-20 18:29:22 -0600

    Subiendo

---
+++ src/data/artists.ts
@@ -0,0 +1,33 @@
+export const artists = [
+    {
+        name: 'Artista 1',
+        slug: 'artista-1',
+        genre: 'Rock',
+        popularity: 'Alta',
+        image: 'https://indiecios.files.wordpress.com/2017/08/musicos-de-sesion-foto-popup.jpg',
+        biography: 'Biografía del Artista 1...',
+        discography: ['Álbum 1', 'Álbum 2'],
+        photos: ['/photo1.jpg', '/photo2.jpg'],
+    },
+    {
+        name: 'Artista 2',
+        slug: 'artista-2',
+        genre: 'Rock',
+        popularity: 'Alta',
+        image: 'https://elsolweb.tv/wp-content/uploads/2018/12/MUSICOS-APLICACIONES.jpg',
+        biography: 'Biografía del Artista 1...',
+        discography: ['Álbum 1', 'Álbum 2'],
+        photos: ['/photo1.jpg', '/photo2.jpg'],
+    },
+    {
+        name: 'Artista 3',
+        slug: 'artista-3',
+        genre: 'Pop',
+        popularity: 'Media',
+        image: 'https://i1.sndcdn.com/artworks-vSDG2SMFoxtV9mPx-7Lx0FA-t500x500.jpg',
+        biography: 'Biografía del Artista 2...',
+        discography: ['Álbum A', 'Álbum B'],
+        photos: ['/photoA.jpg', '/photoB.jpg'],
+    },
+
+];
